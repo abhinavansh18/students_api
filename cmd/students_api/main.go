145add9f68f13f5fce83bc8415e48df6aa4da840
Your Name <your.email@example.com>
2025-06-25 13:43:10 +0530

    added main

--- cmd/students_api/main.go
+++ cmd/students_api/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/abhinavansh18/students_api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to go")
-
+	//load config
+	cfg := config.MustLoad()
+	//load router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+	//setup server
+	server = http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	err := server.ListenandServe()
+	if err != nil {
+		log.Fatal("FAiled to start server")
+	}
+	fmt.Println("Server Started")
 }
