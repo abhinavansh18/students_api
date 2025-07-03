9399d16496c0ca575d4a39f0eb425ba36e5daccd
Your Name <your.email@example.com>
2025-07-03 19:28:42 +0530

    new changes

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,9 +1,20 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+
+	"github.com/abhinavansh18/students_api/internal/types"
+)
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var student types.Student
+
+		json.NewDecoder(r.Body).Decode(&student)
+
+		slog.Info(("Creating a student"))
 		w.Write([]byte("Welcome to students api"))
 
 	}
